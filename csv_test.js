e9a2594a327ceca7147f0c91fed4fd87d6ba1aff
Luisdavidpm <alu0100816518@ull.edu.es>
2016-04-07 16:03:29 +0100

    Añadiendo pruebas mocha y chai

--- csv_test.js
+++ csv_test.js
@@ -1,69 +1,9 @@
 var expect = chai.expect;
 
-describe("Conversor", function() {
-  var sandbox;
-
- beforeEach(function() {
-   sandbox = sinon.sandbox.create();
-   sandbox.stub(window.console, "log");
-   sandbox.stub(window.console, "error");
- });
-
- afterEach(function() {
-   sandbox.restore();
- });
-
-  describe("Medida", function() {
-    it("Cambio de f a c", function() {
-      expect(Medida.convertir("32f to c")).to.equal("0.00 Celsius");
-    });
-    it("Prueba fallo", function() {
-      expect(Medida.convertir("32")).to.equal("Introduzca una temperatura valida: 330e-1 F to C");
-    });
-    it("Prueba conversion no reconocida", function() {
-      //var conv = Medida.convertir('320e-1 f to p');
-      //sinon.assert.notCalled(console.error);
-      //sinon.assert.calledOnce(console.log);
-      //sinon.assert.calledWithExactly(console.log, 'Desconozco como convertir desde "f" hasta "p"');
+  describe("Probando funcion calculate", function() {
+    it("Funcionamiento del CSV", function() {
+      var mensaje = 'producto'
+      var resultado = calculate(mensaje);
+      expect(resultado[0].value[0]).to.equal('producto');
     });
   });
-
-  describe("Temperatura", function() {
-    it("Creacion de un objeto Farenheit", function() {
-      var source = new Farenheit(32);
-      expect(source.valor).to.equal(32);
-    });
-    it("Prueba de Farenheit a Celsius", function() {
-      var source = new Farenheit(32);
-      expect(source.toCelsius()).to.equal(0);
-    });
-    it("Prueba de Farenheit a Kelvin", function() {
-      var source = new Farenheit(32);
-      expect(source.toKelvin()).to.equal(273.15);
-    });
-    it("Creacion de un objeto Celsius", function() {
-      var source = new Celsius(32);
-      expect(source.valor).to.equal(32);
-    });
-    it("Prueba de Celsius a Farenheit", function() {
-      var source = new Celsius(32);
-      expect(source.toFarenheit()).to.equal(89.6);
-    });
-    it("Prueba de Celsius a Kelvin", function() {
-      var source = new Celsius(32);
-      expect(source.toKelvin()).to.equal(305.15);
-    });
-    it("Creacion de un objeto Kelvin", function() {
-      var source = new Kelvin(32);
-      expect(source.valor).to.equal(32);
-    });
-    it("Prueba de Kelvin a Farenheit", function() {
-      var source = new Kelvin(32);
-      expect(source.toFarenheit()).to.equal(-241.14999999999998);
-    });
-    it("Prueba de Kelvin a Celsius", function() {
-      var source = new Kelvin(32);
-      expect(source.toCelsius()).to.equal(-402.07);
-    });
-  });
-});
