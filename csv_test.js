ad827321e25d11920df3005d678284716547e135
alu0100819182 <alu0100819182@ull.edu.es>
2016-04-08 12:17:48 +0100

    Metiendo heroku

--- csv_test.js
+++
@@ -1,9 +0,0 @@
-var expect = chai.expect;
-
-  describe("Probando funcion calculate", function() {
-    it("Funcionamiento del CSV", function() {
-      var mensaje = 'producto'
-      var resultado = calculate(mensaje);
-      expect(resultado[0].value[0]).to.equal('producto');
-    });
-  });
--- server.js
+++ server.js
@@ -6,13 +6,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/Views');
 app.set('view engine', 'ejs');
-
+/*
 app.get('/', function(request, response) {
   response.render('index');
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
