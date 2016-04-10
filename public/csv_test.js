86731cce9d1923fada0959e5253e7feca64a0496
alu0100819182 <alu0100819182@ull.edu.es>
2016-04-10 22:43:42 +0100

    Nuevo

--- public/csv_test.js
+++ public/csv_test.js
@@ -1,6 +1,18 @@
 var expect = chai.expect;
 
   describe("Probando funcion calculate", function() {
+    var sandbox;
+
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(window.console, "log");
+      sandbox.stub(window.console, "error");
+      });
+
+      afterEach(function() {
+        sandbox.restore();
+      });
+
     it("Funcionamiento del CSV", function() {
       var mensaje = 'producto'
       var resultado = calculate(mensaje);
