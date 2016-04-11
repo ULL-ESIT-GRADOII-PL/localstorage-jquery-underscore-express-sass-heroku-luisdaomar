b643eeaf896b10ce6b640d6cb4f07162731d2d8c
alu0100819182 <alu0100819182@ull.edu.es>
2016-04-11 09:52:01 +0100

    Prueba definitiva

--- karma.conf.js
+++ karma.conf.js
@@ -27,8 +27,8 @@ module.exports = function(config) {
       'public/vendor/chai.js',
       'public/vendor/mocha.js',
       'public/vendor/mocha-blanket.js',
-      'public/vendor/sinon-1.12.2',
-      'public/vendor/sinon-1.17.3',
+      'public/vendor/sinon-1.12.2.js',
+      'public/vendor/sinon-1.17.3.js',
       'public/csv.js',
       'public/csv_test.js'
     ],
